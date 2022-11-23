0b977195d0257b4bf5e13161e2474be64acb0a56
hinddaoud <109424272+hinddaoud@users.noreply.github.com>
2022-11-23 22:02:42 +0300

    new design

--- frontend/src/components/Register/index.js
+++ frontend/src/components/Register/index.js
@@ -73,6 +73,10 @@ const Register = () => {
         toast.error("lastName Is Required");}
         if (!email) {
           toast.error("email Is Required");}
+         
+           else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+            toast.error( 'Invalid email address')
+          }
       return;
     }
     axios
@@ -251,100 +255,100 @@ onChange={(e) => {
     </div>
   );
   //-------------return desigin----------------
-  return (
-    <div className=" mainRegisterDiv">
-      <div>
-        <img
-          className="imgRegister"
-          src="./assets/images/pic2.png"
-          alt="pic"
-        ></img>
-      </div>
-      <div className="Form">
-        {true ? (
-          <>
-            <h1>Register</h1>
-            <p>Please enter your Information to Register </p>
-            <hr></hr>
-            <br />
-            <input
-              type="text"
-              placeholder="First Name"
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-            <br />
-            <input
-              type="text"
-              placeholder="Last Name"
-              onChange={(e) => setLastName(e.target.value)}
-            />
-            <br />
-            <input
-              type="number"
-              placeholder="Age"
-              onChange={(e) => setAge(e.target.value)}
-            />
-            <br />
-            <input
-              type="text"
-              placeholder="City"
-              onChange={(e) => setCity(e.target.value)}
-            />
-            <br />
-            {/*  */}
-            <br />
+  // return (
+  //   <div className=" mainRegisterDiv">
+  //     <div>
+  //       <img
+  //         className="imgRegister"
+  //         src="./assets/images/pic2.png"
+  //         alt="pic"
+  //       ></img>
+  //     </div>
+  //     <div className="Form">
+  //       {true ? (
+  //         <>
+  //           <h1>Register</h1>
+  //           <p>Please enter your Information to Register </p>
+  //           <hr></hr>
+  //           <br />
+  //           <input
+  //             type="text"
+  //             placeholder="First Name"
+  //             onChange={(e) => setFirstName(e.target.value)}
+  //           />
+  //           <br />
+  //           <input
+  //             type="text"
+  //             placeholder="Last Name"
+  //             onChange={(e) => setLastName(e.target.value)}
+  //           />
+  //           <br />
+  //           <input
+  //             type="number"
+  //             placeholder="Age"
+  //             onChange={(e) => setAge(e.target.value)}
+  //           />
+  //           <br />
+  //           <input
+  //             type="text"
+  //             placeholder="City"
+  //             onChange={(e) => setCity(e.target.value)}
+  //           />
+  //           <br />
+  //           {/*  */}
+  //           <br />
 
-            <select
-              onChange={(e) => {
-                srtRolrId(e.target.value);
-              }}
-            >
-              <option disabled selected value>
-                {" "}
-                -- select a User Type --{" "}
-              </option>
-              {roles.length > 0 &&
-                roles.map((elem, i) => {
-                  return (
-                    <option
-                      value={elem.id}
+  //           <select
+  //             onChange={(e) => {
+  //               srtRolrId(e.target.value);
+  //             }}
+  //           >
+  //             <option disabled selected value>
+  //               {" "}
+  //               -- select a User Type --{" "}
+  //             </option>
+  //             {roles.length > 0 &&
+  //               roles.map((elem, i) => {
+  //                 return (
+  //                   <option
+  //                     value={elem.id}
 
-                      // textContent={elem.specialty}
-                    >
-                      {elem.role}
-                    </option>
-                  );
-                })}
-            </select>
-            <br />
-            {/*  */}
-            <input
-              type="email"
-              placeholder="Email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <br />
-            <input
-              type="password"
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <br />
-            <button className="registerbtn" onClick={AddNewUse}>
-              Register
-            </button>
-            <br />
+  //                     // textContent={elem.specialty}
+  //                   >
+  //                     {elem.role}
+  //                   </option>
+  //                 );
+  //               })}
+  //           </select>
+  //           <br />
+  //           {/*  */}
+  //           <input
+  //             type="email"
+  //             placeholder="Email"
+  //             onChange={(e) => setEmail(e.target.value)}
+  //           />
+  //           <br />
+  //           <input
+  //             type="password"
+  //             placeholder="Password"
+  //             onChange={(e) => setPassword(e.target.value)}
+  //           />
+  //           <br />
+  //           <button className="registerbtn" onClick={AddNewUse}>
+  //             Register
+  //           </button>
+  //           <br />
 
-            {status
-              ? message && <div className="SuccessMessage">{message}</div>
-              : message && <div className="ErrorMessage">{message}</div>}
-          </>
-        ) : (
-          <p>Logout First</p>
-        )}
-      </div>
-    </div>
-  );
+  //           {status
+  //             ? message && <div className="SuccessMessage">{message}</div>
+  //             : message && <div className="ErrorMessage">{message}</div>}
+  //         </>
+  //       ) : (
+  //         <p>Logout First</p>
+  //       )}
+  //     </div>
+  //   </div>
+  // );
 
   //------------------------------------
 };
